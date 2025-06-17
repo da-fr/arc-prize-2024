d3ac3f6ebf6fb609bfdc782561ee99977ca35d95
Daniel Franzen <dfranzen.it@gmail.com>
2025-06-17 22:29:26 +0200

    added workaround for unsloth's max_seq_length==2048 bug

--- training_code/model_tools.py
+++ training_code/model_tools.py
@@ -41,11 +41,18 @@ class InputMaskingDataCollator(DataCollatorForCompletionOnlyLM):
 
 def load_unsloth_4bit(model_path):
     from unsloth import FastLanguageModel
-    return FastLanguageModel.from_pretrained(
+    model, tokenizer = FastLanguageModel.from_pretrained(
         model_name=model_path,
         dtype=None,
         load_in_4bit=True,
     )
+    if model.max_seq_length == 2048 < model.generation_config.max_length:
+        print(f'CHANGING MAX_SEQ_LENGTH {model.max_seq_length} -> {model.generation_config.max_length} (unsloth bug?)')
+        to_fix = model
+        while to_fix is not None:
+            to_fix.max_seq_length = model.generation_config.max_length
+            to_fix = getattr(to_fix, 'model', None)
+    return model, tokenizer
 
 
 def save_model_and_tokenizer(store_path, model, tokenizer):
